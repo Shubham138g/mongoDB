d1571bdeb42732d1a92fc721c766f4becf862cee
shubham <tonygupta275@gmail.com>
2023-07-26 20:43:32 +0530

    rstfulapi tutorial

--- restfulapi/src/models/Student.js
+++ restfulapi/src/models/Student.js
@@ -1,21 +1,34 @@
-const mongoose=require("mongoose")
-const validator=require("validator")
+const mongoose = require("mongoose")
+const validator = require("validator")
 
-const studentSchema= new mongoose.Schema({
-    name:{
-        type:String,
-        required:true,
-        minlength:3
+const studentSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        minlength: 3
     },
-    email:{
-        type:String,
-        required:true,
-        unique:[true,"email already taken"],
-        validate(value){
-            if(!validator.isEmail(value)){
+    email: {
+        type: String,
+        required: true,
+        unique: [true, "email already taken"],
+        validate(value) {
+            if (!validator.isEmail(value)) {
                 throw new Error("Invalid Error");
             }
         }
+    },
+    phone:{
+        type:Number,
+        required:true,
+        unique:true,
+        maxlength:10,
+        minlength:10
+    },
+    adderess:{
+        type:String,
+        required:true
     }
-
 })
+
+//creating a model
+
