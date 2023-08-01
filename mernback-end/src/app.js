7636fcefa0497a45a2a29cbd9270b16dbe1a98d5
shubham <tonygupta275@gmail.com>
2023-08-01 14:31:25 +0530

    intial commit

--- mernback-end/src/app.js
+++ mernback-end/src/app.js
@@ -1,17 +1,26 @@
 const express=require("express");
 const path=require("path");
 const app =express();
-const PORT=4000;
+const hbs=require("hbs");
+// const PORT=4000;
+const port= process.env.PORT || 3000;
 require("./db/conn");
 
 const static_path= path.join(__dirname,"../public");
+const templates_path= path.join(__dirname,"../templates/views");
+const partials_path= path.join(__dirname,"../templates/partials");
+
+
 app.use(express.static(static_path));
+app.set("views engine","hbs");
+app.set("views",templates_path);
+hbs.registerPartials(partials_path);
 
 app.get("/",(req,res)=>{
-    res.send("hello");
+    res.render("index");
 })
 
 
-app.listen(PORT,()=>{
-     console.log(`server is running on port number ${PORT}`)
+app.listen(port,()=>{
+     console.log(`server is running on port number ${port}`)
 })
