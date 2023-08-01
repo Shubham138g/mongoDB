55360eec93165a9ec799ff221740bebb344c692d
shubham <tonygupta275@gmail.com>
2023-08-01 19:32:42 +0530

    get form data and save to db

--- mernback-end/src/app.js
+++ mernback-end/src/app.js
@@ -1,35 +1,60 @@
-const express=require("express");
-const app =express();
+const express = require("express");
+const app = express();
 
-const path=require("path");
-const hbs=require("hbs");
+const path = require("path");
+const hbs = require("hbs");
 require("./db/conn");
-const Register=require("./models/registers")
+const Register = require("./models/registers")
 // const PORT=4000;
 
-const port= process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 
-const static_path= path.join(__dirname,"../public");
-const templates_path= path.join(__dirname,"../templates/views");
-const partials_path= path.join(__dirname,"../templates/partials");
+const static_path = path.join(__dirname, "../public");
+const templates_path = path.join(__dirname, "../templates/views");
+const partials_path = path.join(__dirname, "../templates/partials");
 
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }));
 
 app.use(express.static(static_path));
-app.set("view engine","hbs");
-app.set("views",templates_path);
+app.set("view engine", "hbs");
+app.set("views", templates_path);
 hbs.registerPartials(partials_path);
 
-app.get("/",(req,res)=>{
+app.get("/", (req, res) => {
     res.render("index");
 })
-app.get("/register",(req,res)=>{
+app.get("/register", (req, res) => {
     res.render("register");
 })
-app.get("/login",(req,res)=>{
+app.post("/register", async (req, res) => {
+    try {
+        const password = req.body.password;
+        const cpassword = req.body.confirm_password;
+        if (password === cpassword) {
+            const registerEmployee = new Register({
+                name: req.body.name,
+                email: req.body.email,
+                phone: req.body.phone,
+                pass: req.body.password,
+                cpass: req.body.confirm_password,
+            })
+
+        }
+        else {
+            res.send("password is not matching")
+        }
+
+    } catch (err) {
+        res.status(400).send();
+    }
+
+})
+app.get("/login", (req, res) => {
     res.render("login");
 })
 
 
-app.listen(port,()=>{
-     console.log(`server is running on port number ${port}`)
+app.listen(port, () => {
+    console.log(`server is running on port number ${port}`)
 })
