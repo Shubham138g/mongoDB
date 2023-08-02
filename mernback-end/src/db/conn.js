e9443d326840543b7a72411d4fcdbcfeb0f51a2d
shubham <tonygupta275@gmail.com>
2023-08-02 20:48:58 +0530

    securing database connection string usinh .env file

--- mernback-end/src/db/conn.js
+++ mernback-end/src/db/conn.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose")
 
-const DB="mongodb+srv://tonygupta275:zoAcRqDwTeldGSC2@cluster0.6efrmtj.mongodb.net/mernbackend?retryWrites=true&w=majority";
+const DB=process.env.DATABASE_NAME;
+// const DB="mongodb+srv://tonygupta275:zoAcRqDwTeldGSC2@cluster0.6efrmtj.mongodb.net/mernbackend?retryWrites=true&w=majority";
 
 
 mongoose.connect(DB,{
